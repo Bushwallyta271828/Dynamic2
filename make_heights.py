44904c5c8c792baabc1d386d8bba00d1301fb472
Bushwallyta <xanhec2001@gmail.com>
2015-08-25 20:41:38 +0100

    Modified make_heights.py
    I added __doc__ strings to the two functions inside, as well as trimming
    the implementation of avg_height_find(...). I made default_samples
    another global parameter at the top of the file.

--- make_heights.py
+++ make_heights.py
@@ -3,15 +3,33 @@ from pylab import *
 
 default_start = 5
 default_stop = 100
+default_samples = int(1e6)
 
-def avg_height_find(n, samples=1000000):
+def avg_height_find(n, samples=default_samples):
+    """
+    Author: Xander
+    This method computes zeta(n), previously 
+    known as barmap[n] inside the compartmentalize
+    function.
+    It makes "samples" different arrays of length
+    n, where each element in each array is a normally
+    distributed variable with mean 0 and standard deviation
+    1. It computes the average of max(array) - min(array)
+    for each array in our set of "samples" arrays.
+    It heavily relies on numpy.
+    """
     data = normal(0, 1, (n, samples))
-    maxs = amax(data, axis=0)
-    mins = amin(data, axis=0)
-    heights = maxs - mins
-    return average(heights)
+    return average(amax(data, axis=0)
+                 - amin(data, axis=0))
     
 def make_file(start=default_start, stop=default_stop):
+    """
+    Author: Xander
+    This method calls avg_height_find(...) for every n
+    in the range [start, stop], including stop, and writes
+    the output to heights.txt in the format:
+        n:zeta(n)
+    """
     avg_heights = {}
     f = open("heights.txt", "w")
     for i in avg_heights:
