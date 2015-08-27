817714e2cbac040b85d394029f450e3ef13745b7
Bushwallyta271828 <bushwallyta@gmail.com>
2015-08-27 11:47:41 +0100

    Added make_image.py

---
+++ make_image.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import division
+from pylab import *
+from numpy import *
+from program import *
+
+def create_image():
+    
+    
+if __name__=="__main__":
+    create_image()
