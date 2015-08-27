8db9336d9c488c57ad1e2c8710550e2ca425e0d2
Bushwallyta271828 <bushwallyta@gmail.com>
2015-08-27 11:51:46 +0100

    Worked on make_image.py

--- make_image.py
+++ make_image.py
@@ -2,10 +2,27 @@
 from __future__ import division
 from pylab import *
 from numpy import *
+from matplotlib.pyplot import plot, show
 from program import *
 
 def create_image():
-    
+    lightcurve = normal(0, 1, (1000,))
+    compartmentalization = compartmentalize(lightcurve, max_pval=0.2)
+    plot(lightcurve)
+    lowers = []
+    uppers = []
+    xs = []
+    for pointnum, point in enumerate(compartmentalization[:-1]):
+        next_point = compartmentalization[pointnum + 1]
+        block = lightcurve[point: next_point]
+        minimum = min(block)
+        maximum = max(block)
+        xs += [point, next_point - 1]
+        lowers += [minimum, minimum]
+        uppers += [maximum, maximum]
+    plot(xs, lowers)
+    plot(xs, uppers)
+    show()
     
 if __name__=="__main__":
     create_image()
