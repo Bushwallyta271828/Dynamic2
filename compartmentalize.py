aa24debfcb7a5b596a7b796a71c202b4d3c1c582
Bushwallyta271828 <bushwallyta@gmail.com>
2015-08-28 16:22:46 +0100

    updated compartmentalize imports

--- compartmentalize.py
+++ compartmentalize.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import division
 from pylab import *
-from matplotlib import cm
-from scipy.stats import norm
-from astropy.io import fits
+from numpy import *
 
 """
 Comments:
